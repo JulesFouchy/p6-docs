b528099791237e922ee77691a365abb316e426ad
Jules Fouchy <jules.fouchy@ntymail.com>
2021-11-11 03:55:54 +0100

    [p6] Update

--- main.cpp
+++ main.cpp
@@ -4,10 +4,26 @@
 int main()
 {
     auto p6 = p6::Context{{1280, 720, "p6 example"}};
-    p6.set_time_mode_fixedstep();
+    // p6.set_time_mode_fixedstep();
     p6.update = [&]() {
         p6.background({0.5f, 0.3f, 0.8f});
-        std::cout << 1.f / p6.delta_time() << '\n';
+        p6.rect();
+        // std::cout << 1.f / p6.delta_time() << '\n';
+    };
+    p6.mouse_moved = [&](p6::MouseMove event) {
+        std::cout << "MOVED\n";
+        // std::cout << event.position.x << " " << event.position.y << '\n';
+        // std::cout << event.delta.x << " " << event.delta.y << '\n'
+        //           << "*********\n";
+    };
+    p6.mouse_dragged = [&](p6::MouseDrag event) {
+        std::cout << "DRAGGED\n";
+    };
+    p6.mouse_pressed = [](p6::MouseButton event) {
+        std::cout << "PRESSED\n";
+    };
+    p6.mouse_released = [](p6::MouseButton event) {
+        std::cout << "RELEASED\n";
     };
     p6.run();
     return 0;
