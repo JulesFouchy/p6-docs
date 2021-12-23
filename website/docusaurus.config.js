17a5c4ee3bdf1ae6a9ddb6d32394f4b7237182f2
Jules Fouchy <jules.fouchy@ntymail.com>
2021-12-23 17:08:40 +0100

    [website] Start

---
+++ website/docusaurus.config.js
@@ -0,0 +1,113 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'p6',
+  tagline: 'Making 🎨 with 👩‍💻',
+  url: 'https://julesfouchy.github.io/',
+  baseUrl: '/p6-examples/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'julesfouchy',
+  projectName: 'p6-examples',
+  themeConfig: {
+    navbar: {
+      title: '',
+      logo: {
+        alt: 'Site Logo',
+        src: 'img/favicon-32x32.png',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'tutorials/introduction',
+          position: 'left',
+          label: 'Tutorials',
+        },
+        {
+          type: 'doc',
+          docId: 'api/introduction',
+          position: 'left',
+          label: 'API',
+        },
+        {
+          href: 'https://github.com/JulesFouchy/p6-examples/issues',
+          position: 'right',
+          className: 'header-issues-link',
+          'aria-label': 'GitHub issues',
+        },
+        {
+          href: 'https://github.com/JulesFouchy/p6-examples',
+          position: 'right',
+          className: 'header-github-link',
+          'aria-label': 'GitHub repository',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Raise an issue!',
+          items: [
+            {
+              label: 'Ask a question',
+              to: 'https://github.com/JulesFouchy/p6-examples/issues',
+            },
+            {
+              label: 'Report a mistake',
+              to: 'https://github.com/JulesFouchy/p6-examples/issues',
+            },
+            {
+              label: 'Give us feedback',
+              to: 'https://github.com/JulesFouchy/p6-examples/issues',
+            },
+          ],
+        },
+        {
+          title: 'Contact Us',
+          items: [
+            {
+              label: 'Discord',
+              to: 'https://discord.com/users/372812330742054914',
+            },
+            {
+              label: 'E-Mail',
+              to: 'mailto:jules.fouchy@ntymail.com',
+            },
+            {
+              label: 'GitHub',
+              to: 'https://github.com/JulesFouchy',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${
+          new Date()
+              .getFullYear()}. Built with <a href="https://docusaurus.io/">Docusaurus</a>.
+              <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+              `,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          path: 'docs',
+          routeBasePath: 'docs',
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
