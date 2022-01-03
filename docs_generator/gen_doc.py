fa6d3237133d8ad8ab01a9330f60d2f411e5367e
Jules Fouchy <jules.fouchy@ntymail.com>
2022-01-03 03:13:31 +0100

    [docs_generator] Fix <br> tags

--- docs_generator/gen_doc.py
+++ docs_generator/gen_doc.py
@@ -17,6 +17,6 @@ def generate_documentation(output_folder):
         os.remove(filename)
 
     # Run doxybook2
-    os.system(f"doxybook2.exe --input Doxygen/xml --output {output_folder} --config doxybook_config.json")
+    os.system(f"doxybook2.exe --input Doxygen/xml --output {output_folder} --config doxybook_config.json --templates doxybook_templates")
 
 generate_documentation(output_folder = "../website/docs/api")
