78e42875152d2d621771ab404a8254bdb0dc3630
Jules Fouchy <jules.fouchy@ntymail.com>
2022-02-02 16:12:03 +0100

    [tests::square_and_rectangle] Cleanup

--- tests/square_and_rectangle.cpp
+++ tests/square_and_rectangle.cpp
@@ -11,23 +11,28 @@ int main()
         ctx.background({});
         ctx.stroke = {1, 1, 1, 1};
         // White square positioned by Center
-        ctx.square(p6::Center{ctx.mouse()}, p6::Radius{0.3f},
+        ctx.square(p6::Center{ctx.mouse()},
+                   p6::Radius{0.3f},
                    p6::Rotation{rotation});
         // Red square positioned by Top Left Corner
         ctx.fill = {1, 0, 0, 0.5};
-        ctx.square(p6::TopLeftCorner{ctx.mouse()}, p6::Radius{0.3f},
+        ctx.square(p6::TopLeftCorner{ctx.mouse()},
+                   p6::Radius{0.3f},
                    p6::Rotation{rotation});
         // Green square positioned by Top Right Corner
         ctx.fill = {0, 1, 0, 0.5};
-        ctx.square(p6::TopRightCorner{ctx.mouse()}, p6::Radius{0.3f},
+        ctx.square(p6::TopRightCorner{ctx.mouse()},
+                   p6::Radius{0.3f},
                    p6::Rotation{rotation});
         // Blue square positioned by Bottom Left Corner
         ctx.fill = {0, 0, 1, 0.5};
-        ctx.square(p6::BottomLeftCorner{ctx.mouse()}, p6::Radius{0.3f},
+        ctx.square(p6::BottomLeftCorner{ctx.mouse()},
+                   p6::Radius{0.3f},
                    p6::Rotation{rotation});
         // Cyan square positioned by Bottom Right Corner
         ctx.fill = {0, 1, 1, 0.5};
-        ctx.square(p6::BottomRightCorner{ctx.mouse()}, p6::Radius{0.3f},
+        ctx.square(p6::BottomRightCorner{ctx.mouse()},
+                   p6::Radius{0.3f},
                    p6::Rotation{rotation});
     };
     ctx.start();
