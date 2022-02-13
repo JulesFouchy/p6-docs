8326cd5388fb32d02712b1a1c7c5d3d178939c7e
Jules Fouchy <jules.fouchy@ntymail.com>
2022-02-13 18:02:23 +0100

    [tests] Add render_to_image

---
+++ tests/render_to_image.cpp
@@ -0,0 +1,31 @@
+#include <p6/p6.h>
+
+int main()
+{
+    auto ctx   = p6::Context{{1280, 720, "Render to Image"}};
+    auto image = p6::Image{ctx.window_size()}; // Create an empty image with the same size as the window
+    ctx.render_to_image(image);
+    ctx.background({0.3f, 0.5f, 0.9f, 0.5f});
+    ctx.render_to_screen();
+    ctx.window_resized = [&]() {
+        image.resize(ctx.window_size()); // Make sure that the image still has the same size as the window even after it resizes
+    };
+    const auto brush = [&]() {
+        ctx.use_stroke = false;
+        ctx.fill       = {1.f, 0.7f, 0.2f, 0.3f};
+        ctx.circle(p6::Center{ctx.mouse()}, p6::Radius{0.3f});
+    };
+    ctx.update = [&]() {
+        ctx.background({1, ctx.shift() ? 1.f : 0.f, 0});
+        ctx.image(image);
+        if (ctx.ctrl()) {
+            ctx.render_to_image(image);
+            brush(); // We can do some permanent drawings on our image
+            ctx.render_to_screen();
+        }
+        else {
+            brush(); // While still being able to draw temporary things on the screen
+        }
+    };
+    ctx.start();
+}
