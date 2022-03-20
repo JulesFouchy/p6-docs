bb6d010b3817ba1fe828491f6008512cd0d4171f
Jules Fouchy <jules.fouchy@ntymail.com>
2022-03-20 18:04:53 +0100

    [p6] Add text()

---
+++ tests/text.cpp
@@ -0,0 +1,22 @@
+#include <exe_path/exe_path.h>
+#include <p6/p6.h>
+#include <filesystem>
+#include <iostream>
+
+using namespace std;
+using namespace filesystem;
+int main()
+{
+    auto ctx   = p6::Context{{1280, 720, "Text"}};
+    ctx.update = [&]() {
+        ctx.background(p6::NamedColor::PinkPearl);
+        ctx.fill = p6::NamedColor::OrangePeel;
+        ctx.circle(p6::Center{ctx.mouse()}, p6::Radius{0.3f});
+        ctx.text_size = 0.1f;
+        ctx.fill      = {1.f, 0.f, 0.f, 1.f};
+        ctx.text(u"abcdefghijklmnop", p6::Center{});
+        ctx.fill = {1.f, 0.f, 0.f, 0.5f};
+        ctx.text(u"qrstuvwxyz", p6::Center{0.f, -2.f * ctx.text_size});
+    };
+    ctx.start();
+}
