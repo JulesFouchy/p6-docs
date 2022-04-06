c66e8df79b896eb65c0f82db20cb487f487a9050
Jules Fouchy <jules.fouchy@ntymail.com>
2022-04-06 22:09:02 +0200

    [tests::text] Test more characters

--- tests/text.cpp
+++ tests/text.cpp
@@ -14,9 +14,13 @@ int main()
         ctx.circle(p6::Center{ctx.mouse()}, p6::Radius{0.3f});
         ctx.text_size = 0.1f;
         ctx.fill      = {1.f, 0.f, 0.f, 1.f};
-        ctx.text(u"abcdefghijklmnop", p6::Center{});
+        ctx.text(u"abcdefghijklmnop", p6::Center{0.f, 2.f * ctx.text_size});
         ctx.fill = {1.f, 0.f, 0.f, 0.5f};
-        ctx.text(u"qrstuvwxyz", p6::Center{0.f, -2.f * ctx.text_size});
+        ctx.text(u"qrstuvwxyz", p6::Center{0.f, 0.f * ctx.text_size});
+        ctx.text(u"12345 6789", p6::Center{0.f, -2.f * ctx.text_size});
+        ctx.text(u"!\\\"#$%&\'", p6::Center{0.f, -4.f * ctx.text_size});
+        ctx.text(u",()*+-./:;", p6::Center{0.f, -6.f * ctx.text_size});
+        ctx.text(u"<=>?@[]^_`{|}~", p6::Center{0.f, -8.f * ctx.text_size});
     };
     ctx.start();
 }
