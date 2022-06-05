34bf27ab0ea6d9d6696d126798a6bd6f64c151df
Jules Fouchy <jules.fouchy@ntymail.com>
2022-06-05 12:06:23 +0200

    ✅ [tests::render_to_canvas] Test with a canvas that doesn't have the same aspect ratio as the window

--- tests/render_to_canvas.cpp
+++ tests/render_to_canvas.cpp
@@ -3,16 +3,21 @@
 int main()
 {
     auto ctx    = p6::Context{{1280, 720, "Render to Canvas"}};
-    auto canvas = p6::Canvas{ctx.framebuffer_size()}; // Create a canvas with the same size as the window's framebuffer
+    auto canvas = p6::Canvas{{2 * ctx.framebuffer_height(), // Create a canvas with a size relative to the the window's framebuffer size
+                              2 * ctx.framebuffer_height()}};
+
+    ctx.framebuffer_resized = [&]() {
+        canvas.resize({2 * ctx.framebuffer_height(), // Make sure that the canvas still has the same relative size after the framebuffer resizes
+                       2 * ctx.framebuffer_height()});
+    };
+
     ctx.render_to_canvas(canvas);
     ctx.background({0.3f, 0.5f, 0.9f, 0.5f});
     ctx.render_to_screen();
-    ctx.framebuffer_resized = [&]() {
-        canvas.resize(ctx.framebuffer_size()); // Make sure that the canvas still has the same size as the framebuffer even after it resizes
-    };
+
     const auto brush = [&]() {
         ctx.use_stroke = false;
-        ctx.fill       = {1.f, 0.7f, 0.2f, 0.3f};
+        ctx.fill       = {1.f, 1.f, 1.f, 0.5f};
         ctx.circle(p6::Center{ctx.mouse()}, p6::Radius{0.05f});
     };
     ctx.update = [&]() {
@@ -24,10 +29,7 @@ int main()
             brush(); // We can do some permanent drawings on our canvas
             ctx.render_to_screen();
         }
-        else
-        {
-            brush(); // While still being able to draw temporary things on the screen
-        }
+        brush(); // While still being able to draw temporary things on the screen
     };
     ctx.start();
 }
